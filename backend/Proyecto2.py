9c4ae16f32588f10cd47d81c927f2c44cfd6dc99
josue <Josuesoli121201@gmail.com>
2021-11-07 17:40:38 -0600

    final

--- backend/Proyecto2.py
+++ backend/Proyecto2.py
@@ -13,6 +13,10 @@ from flask_cors import CORS
 app = Flask (__name__)
 CORS(app)
 
+@app.route('/',methods=['GET'])
+def defau():
+    return jsonify({"api":"202001574"})
+
 @app.route('/login', methods=['POST'])
 def loginRoute():
     response = login(request)
